626e040845139808f44fc5990fe48c507720a05a
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-04-16 01:42:30 +0300

    feat(styles): update tailwindcss config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,10 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.svelte"],
   darkMode: ["class"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      screens: { xs: "512px" },
+    },
+  },
   plugins: [],
 } satisfies Config;
