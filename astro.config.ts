e6482292854bf221fb7cb61c51fe450c3f429df5
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-04-17 14:51:24 +0300

    chore: add tailwindcss integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,16 @@
 import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [svelte({ compilerOptions: { runes: true } })],
+  integrations: [
+    svelte({
+      compilerOptions: {
+        runes: true,
+      },
+    }),
+    tailwind({ applyBaseStyles: false, nesting: true }),
+  ],
 });
