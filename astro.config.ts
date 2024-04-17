77cde6d8e6cc602c3b405f0d82eb3b25b8042880
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-04-17 15:14:29 +0300

    chore: add db integration

--- astro.config.ts
+++ astro.config.ts
@@ -2,15 +2,21 @@ import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [
+    db(),
     svelte({
       compilerOptions: {
         runes: true,
       },
     }),
-    tailwind({ applyBaseStyles: false, nesting: true }),
+    tailwind({
+      applyBaseStyles: false,
+      nesting: true,
+    }),
   ],
 });
