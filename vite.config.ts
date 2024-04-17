cc53f7b3d5d20c66349fd14b76cc701c479c0277
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-04-17 14:21:25 +0300

    chore: remove vite config

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { defineConfig } from "vite";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/todo-list/",
-  plugins: [svelte({ compilerOptions: { runes: true } })],
-});
