2a3653b81f3082ccffbe2372b30043f684ce7c2c
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-04-22 07:41:05 +0300

    feat(db): configure database
    Define Task table.

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,18 @@
-import { defineDb } from 'astro:db';
-
 // https://astro.build/db/config
+
+import { column, defineDb, defineTable } from "astro:db";
+
+const Task = defineTable({
+  columns: {
+    name: column.text({ unique: true }),
+    id: column.number({ primaryKey: true, unique: true }),
+    dueDate: column.date({ optional: true }),
+    priority: column.text({ optional: true }),
+    completed: column.boolean({ default: false }),
+    notes: column.text({ optional: true }),
+  },
+});
+
 export default defineDb({
-  tables: {}
+  tables: { Task },
 });
