0ce81bfa6d8a7cd4c6967a1ea636982717253a80
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-04-22 07:43:22 +0300

    chore: add vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,19 +1,16 @@
-import svelte from "@astrojs/svelte";
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
 import db from "@astrojs/db";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: vercel(),
   integrations: [
     db(),
-    svelte({
-      compilerOptions: {
-        runes: true,
-      },
-    }),
+    svelte(),
     tailwind({
       applyBaseStyles: false,
       nesting: true,
