fbdd3cf17aef80196407d3515b1101b6aa4dd79b
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-05-11 23:52:13 +0300

    feat(components): update task details

--- src/components/task_details.svelte
+++ src/components/task_details.svelte
@@ -1,24 +1,36 @@
 <script lang="ts">
+  import { intlFormat, intlFormatDistance, isPast } from "date-fns";
   interface Props {
-    task: Todo | null;
-    show: boolean;
+    task: Task | undefined;
+    showDetails: boolean;
   }
 
-  let { task, show }: Props = $props();
+  let { task, showDetails }: Props = $props();
 </script>
 
 <div
-  class={`w-full xs:w-3/5 md:w-1/2 lg:w-1/3 h-dvh py-3 px-4 bg-gray-300 ${show ? "flex" : "hidden"}`}
+  class={`w-full xs:w-3/5 md:w-1/2 lg:w-1/3 h-dvh py-3 px-4 bg-gray-300 ${showDetails ? "flex" : "hidden"}`}
 >
   <dl class="w-full space-y-2">
     <div class="flex items-center gap-2 py-2 px-3 rounded-md bg-gray-200">
-      <dt>
+      <dt class="flex items-center">
         <span class="icon icon-calendar"></span>
         <span class="sr-only">Due date</span>
       </dt>
-      <dd>Due {task?.dueDate?.toDateString()}</dd>
+      <dd
+        class={` ${task?.dueDate && isPast(task?.dueDate) ? "text-red-500" : ""}`}
+        title={task?.dueDate && isPast(task.dueDate)
+          ? `Due ${intlFormatDistance(task?.dueDate, new Date())}`
+          : ""}
+      >
+        Due {task?.dueDate
+          ? intlFormat(task?.dueDate, {
+              dateStyle: "medium",
+            })
+          : ""}
+      </dd>
     </div>
-    
+
     <div class="flex items-center gap-2 py-2 px-3 rounded-md bg-gray-200">
       <dt>
         <span class="sr-only">Priority</span>
@@ -27,8 +39,9 @@
       <dd>{task?.priority}</dd>
     </div>
     <div class="flex items-center gap-2 py-2 px-3 rounded-md bg-gray-200">
-      <dt>
-        <span class="sr-only">Notes</span>
+      <dt class="flex items-center gap-2">
+        <span class="icon icon-sticky-note"></span>
+        <span class="">Notes</span>
       </dt>
       <dd>{task?.notes}</dd>
     </div>
