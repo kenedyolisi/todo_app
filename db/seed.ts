4478d9f02f6b5f7ea249fa1c517429f3990f6326
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-05-11 12:47:08 +0300

    feat(data): update seeding data

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,29 @@
-import { db } from 'astro:db';
+import { db, Task } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  try {
+    await db.insert(Task).values([
+      {
+        name: "Pay bills",
+        dueDate: new Date(2024, 3, 13),
+        priority: "low",
+        completed: false,
+      },
+      {
+        name: "Mow lawn",
+        dueDate: new Date(),
+        priority: "medium",
+        completed: true,
+      },
+      {
+        name: "Read book",
+        priority: "high",
+        completed: false,
+      },
+    ]);
+  } catch (error) {
+    console.log("Database error", error);
+    throw new Error("Failed to seed database!");
+  }
 }
