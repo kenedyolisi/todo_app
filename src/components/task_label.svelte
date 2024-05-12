a09dd34dac7eb01092b69190814e07f44552c0d9
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-05-12 08:44:45 +0300

    feat(components): create task label

---
+++ src/components/task_label.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  interface Props {
+    task: Task;
+  }
+  let { task }: Props = $props();
+</script>
+
+<form>
+  <label class="flex items-center gap-2">
+    <input
+      class="w-5 h-5"
+      name="name"
+      type="checkbox"
+      bind:checked={task.completed}
+    />
+    {#if task.completed}
+      <s> {task.name} </s>
+    {:else}
+      {task.name}
+    {/if}
+  </label>
+</form>
