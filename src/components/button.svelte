ff62bbd84bb698845670b2fcf4d7544c04324dbd
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-06-03 15:32:00 +0300

    feat(components): create button.svelte

---
+++ src/components/button.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type {HTMLButtonAttributes } from "svelte/elements";
+
+  interface Props extends HTMLButtonAttributes {}
+
+  let {children, class:classList, type = "button", ...attributes }: Props = $props();
+
+</script>
+
+<button class={`rounded-md bg-primary focus-within:ring-4 focus-within:ring-offset-2 ${classList}`} {type} {...attributes} >
+{@render children()}
+</button>
